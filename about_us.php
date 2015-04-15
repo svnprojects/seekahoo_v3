b9992ea7840dbb7c6b428b3f2de731921f562e94
thiruboss <thiru.xvalue@gmail.com>
2015-04-15 18:41:19 +0530

    thiru

---
+++ about_us.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html" />
+      <title>SEEKAHOO</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" type="image/jpg" href="images/app_icon_58x58.png"/>
+      <link rel="shortcut icon" type="image/jpg" href="images/app_icon_58x58.png"/>
+      <link rel="stylesheet" href="css/bootstrap.css">
+      <link rel="stylesheet" href="css/bootstrap-theme.css">
+      <script src="js/jquery-1.11.1.js"></script>
+      <script src="js/bootstrap-3.2.0.js"></script>
+      <link rel="stylesheet" href="css/style.css">
+      <style> 
+         #space23 {
+         border: 1px solid #c1cad6;
+         padding: 10px 40px; 
+         background: #ffffff;
+         border-radius: 5px;
+         }
+         #space33 {
+         border: 1px solid #c1cad6;
+         padding: 10px 40px; 
+         background: #ffffff;
+         margin:0 auto;
+         border-radius: 5px;
+         }
+         #wrap {  
+         min-height: 100%;
+         height: auto !important;
+         height: 100%;
+         /* Negative indent footer by it's height */
+         margin: 0 auto -60px;
+         }
+         #submit-support-request{
+         padding: 10px 0px;
+         border: none;
+         color: white;
+         line-height: 20px;
+         background: #28a3db;
+         font-size: 15px;
+         text-transform: uppercase;
+         width:60%;
+         margin:0 auto;
+         float:center;
+         line-height: 30px;
+         }
+         input{
+         margin-bottom: 10px;
+         height: 40px;
+         font-size:17px;
+         line-height: 19px;
+         color: #4b505e;
+         border-color: #c1cad6;
+         }
+      </style>
+   
+    <?php include_once("analyticstracking.php") ?>  
+   </head>
+   <body>
+      <?php include_once("analyticstracking.php") ?>
+      <div id="wrap">
+         <div class="" style="padding:0px 0 0 0;">
+            <?php include("header_contact.php"); ?>
+         </div>
+         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding:1px;background-color:#fc9a30"></div>
+         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" class="img-responsive" style="background-image: url('images/bg_123.jpg');
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;padding-bottom:65px;" >
+            <div class="col-sm-12 col-md-12 col-lg-12" style="margin-top:60px;margin-bottom:20px;">
+               <h1 style="text-align:center; font-family:Robo-bold;font-size:40px;">Who we are…</h1>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-12" align="center" style="margin-top:30px;margin-bottom:60px;">
+               <div id="space23" class="col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-sm-8 col-md-8 col-lg-8">
+                  <p style="margin: 30px 0 10px;line-height: 1.42857143;font-size:17px;font-family:Robo-Regular;text-align:left;color: #4b505e;">Seekahoo is small company based out of Pittsburgh, PA.  The idea originated like most do, by running into the same problem over and over again and finally realizing there has to be a better way. </p>
+                  <p style="margin: 10px 0 10px;line-height: 1.42857143;font-size:17px;font-family:Robo-Regular;text-align:left;color: #4b505e;">As an electrical contractor, I found advertising my business cumbersome.  I wanted my work to speak for itself. When I began sharing my frustration with friends and family, I heard many complaints from the other perspective, about how hard it is to find reliable contractors. What’s unfortunate is even the most popular review sites (we won’t mention any names) skew and finagle search results, deceiving people every day. </p>
+                  <p style="margin: 10px 0 10px;line-height: 1.42857143;font-size:17px;font-family:Robo-Regular;text-align:left;color: #4b505e;">I started seeing a clear disconnect between the contractor and consumer interaction. It was obvious - who better to advertise your work than your own customers? And who better to refer to your friends and family than a contractor you have experience with? This brings honesty, integrity, and reliability, which are the keys to a great product and experience.</p>
+                  <p style="margin: 10px 0 10px;line-height: 1.42857143;font-size:17px;font-family:Robo-Regular;text-align:left;color: #4b505e;">That’s Seekahoo. It’s hoo you know…  </p>
+                  <p style="margin: 10px 0 30px;line-height: 1.42857143;font-size:17px;font-family:Robo-Regular;text-align:left;color: #4b505e;">If you ever want to learn more about us or our company send us an email at <a href="#" style="color:#136a9d;font-size:17;text-decoration:underline;">support@seekahoo.com</a> or fill out our contact form.  </p>
+                  <p style="margin: 10px 0 30px;line-height: 1.42857143;font-size:17px;font-family:Robo-Regular;text-align:right;color: #4b505e;"> - Zack Parme, Co-founder    </p>
+                  <p style="margin: 10px 0 30px;line-height: 1.42857143;font-size:17px;font-family:Robo-Regular;text-align:left;color: #4b505e;">P.S. Our search results can’t be bought. Seekahoo is free for people and Pros alike providing a better experience for everyone.</p>
+               </div>
+            </div>
+         </div >
+         <?php include("footer.php"); ?>
+      </div>
+	  
+	  
+	  
+  </body>
+</html>
